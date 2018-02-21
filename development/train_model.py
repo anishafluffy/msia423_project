08d52ba44a4df701444c377a168a54783e324014
Anisha Dubhashi <anisha.dubhashi@gmail.com>
2018-02-21 08:41:11 -0600

    html and .py for local app

---
+++ development/train_model.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import nltk
+import re
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+import pickle
+from sklearn.externals import joblib
+
+from clean_data import clean_data
+
+def train_model(training_df):
+    #instantiate vectorizer
+    vect = TfidfVectorizer(stop_words = 'english', #remove standard english stop words
+                           ngram_range = (1 , 1), #range of n-values for different n-grams to be extracted
+                           analyzer = "word", #feature made of word (not character) n-grams
+                           max_df = .57, #ignore terms that have a document frequency higher than threshold 
+                           binary = False , 
+                           token_pattern = r'\w+' , #what constitutes a “token”
+                           sublinear_tf = False
+                          )
+    #use vectorizer on training corpus
+    predictors = vect.fit_transform(df['ingredients_string']).todense()
+    #define response
+    response = df['cuisine']
+    #instantiate model
+    clf = LogisticRegression()
+    #train the model
+    parameters = {'C':[1, 10]}
+    classifier = GridSearchCV(clf, parameters)
+    classifier = classifier.fit(predictors, response) 
+    # return vect, classifier
+
+    #create pickle files
+    joblib.dump(vect, 've.pkl') 
+    joblib.dump(classifier, 'cl.pkl') 
+
+# if __name__ == “__main__“:
+#    model(clean_data.clean_data())
+
