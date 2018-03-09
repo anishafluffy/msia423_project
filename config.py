b2dec021ae89e4bf0d6e57eb6ae7ea2cc1ded847
Anisha Dubhashi <anisha.dubhashi@gmail.com>
2018-03-09 14:33:34 -0600

    update config

--- config.py
+++ config.py
@@ -1,13 +1,26 @@
 # config.py
 
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    #SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI ='sqlite:///' + os.path.join(basedir, 'application.db')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://anisha:Cashews16@aaya3n98crhl0o.cm6i6jyhr2zm.us-east-2.rds.amazonaws.com/ebdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# Local database
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///../../data/data.sqlite'
+#LOCAL_PATH = '../data/data.sqlite'
+#DB_PATH = '../../data/database.sqlite'
+
+# RDS database
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://anisha:msia_423@ebdb.cm6i6jyhr2zm.us-east-2.rds.amazonaws.com:3306/msia_db'
+
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+#SECRET_KEY = 'sbQm38a1awYXbmfWLgZaHCupa8egevTiZusvHx/G'
+
+
+# class Config(object):
+#     #SECRET_KEY = os.environ.get('SECRET_KEY')
+#     #SQLALCHEMY_DATABASE_URI ='sqlite:///' + os.path.join(basedir, 'application.db')
+#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://anisha:Cashews16@aaya3n98crhl0o.cm6i6jyhr2zm.us-east-2.rds.amazonaws.com/ebdb'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Enable Flask's debugging features. Should be False in production
 #DEBUG = True
@@ -16,4 +29,5 @@ class Config(object):
 #SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/tracks.db'
 
 #SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
