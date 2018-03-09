9f2397eb29a52a931ab9f48578a0b004a1069d79
Anisha Dubhashi <anisha.dubhashi@gmail.com>
2018-03-09 14:13:05 -0600

    eb

--- create_db.py
+++ create_db.py
@@ -7,15 +7,15 @@ Creates a table in the database provided as the 'SQLALCHEMY_DATABASE_URI'
 configuration parameter in __init__.py with the schema defined by models.Ing()
 '''
 
-#def create_db():
-db.create_all()
+def create_db():
+	db.create_all()
     # print('2 creating')
     # ing1 = Ing(i_ingredients='tortillas, cheese')
     # ing2 = Ing(i_ingredients='curry paste, basil')
     # db.session.add(ing1)
     # db.session.add(ing2)
     # db.session.commit()
-print('db created')
+	print('db created')
 
-# if __name__ == "__main__":
-#     create_db()
+if __name__ == "__main__":
+	create_db()
